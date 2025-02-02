8a58e54764a3a6ba71ede668e48677d71ce32bcd
TG360 <ritinmereddy@gmail.com>
2025-02-02 16:11:36 -0600

    Learning Swift, Section 1-4

---
+++ Srumdinger/Srumdinger/SrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SrumdingerApp.swift
+//  Srumdinger
+//
+//  Created by Ritin Mereddy on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
---
+++ Srumdinger/SrumdingerTests/SrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SrumdingerTests.swift
+//  SrumdingerTests
+//
+//  Created by Ritin Mereddy on 2/2/25.
+//
+
+import Testing
+@testable import Srumdinger
+
+struct SrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
